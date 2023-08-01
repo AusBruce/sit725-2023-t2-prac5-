3a58c66417e73ec6d1bd90c2d287cf390456ecc7
stephenliu8964 <dq89_64@hotmail.com>
2023-08-01 14:05:19 +1000

    getting it ready for p4

--- server.js
+++ server.js
@@ -3,17 +3,11 @@ let app = express();
 let port = process.env.port || 3000;
 
 app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', function (req,res) {
     res.render('index.html');
-});
-
-app.get('/addTwoNumbers', function (req,res) {
-    let num1 = req.query.num1;
-    let num2 = req.query.num2;
-    let result = parseInt(num1) + parseInt(num2);
-    let response = {data:result, message:'sucess', statusCode:200}
-    res.json(response);
 });
 
 app.listen(port, ()=>{
